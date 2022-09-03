214bef1f5af4ebe55668f04e2dec500922b79ecc
Renan Rodrigues <111028127+RenanRodrigues01@users.noreply.github.com>
2022-09-03 11:51:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const botaomenu = document.querySelector('.cabecalho__menu')
+const  menu = document.querySelector('.menu-lateral')
+
+botaomenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral--ativo')
+}) 
